d701bc33935e8c79e11bce221630752d95bd7a8f
Allison Stewart <amstewart@yahoo.com>
2021-12-17 08:44:34 -0500

    project set up and homepage linked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link, Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <div className='nav-links'>
+        <Link id='order-pizza' to='/'>Home</Link>
+      </div>
+      <Switch>
+      
+      </Switch>
+    </div>
   );
 };
 export default App;
